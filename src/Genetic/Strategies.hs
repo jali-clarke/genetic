e55dde141f5d522dcaa483fb3e1b5e741afe0de7
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-06-19 15:14:24 +0000

    add rouletteSelect strategy

--- src/Genetic/Strategies.hs
+++ src/Genetic/Strategies.hs
@@ -1,5 +1,6 @@
 module Genetic.Strategies
-  ( tournamentSelect,
+  ( rouletteSelect,
+    tournamentSelect,
   )
 where
 
@@ -7,11 +8,36 @@ import Control.Monad.Random.Class (MonadRandom (getRandomR))
 import Data.Vector (Vector)
 import qualified Data.Vector as Vector
 
+rouletteSelect :: MonadRandom m => Vector (a, Double) -> m a
+rouletteSelect populationWithFitness =
+  let minFitness = Vector.minimum (fmap snd populationWithFitness)
+      normalizedPopulationWithFitness =
+        if minFitness <= 0
+          then fmap (\(member, fitness) -> (member, fitness + abs minFitness)) populationWithFitness
+          else populationWithFitness
+      totalFitness = Vector.sum $ fmap snd normalizedPopulationWithFitness
+   in if totalFitness == 0
+        then fst <$> chooseVectorUniform normalizedPopulationWithFitness
+        else do
+          threshold <- getRandomR (0, totalFitness)
+          pure $ chooseThreshold threshold normalizedPopulationWithFitness
+
 tournamentSelect :: MonadRandom m => Vector (a, Double) -> m a
 tournamentSelect populationWithFitness = do
-  (member0, fitness0) <- chooseVector populationWithFitness
-  (member1, fitness1) <- chooseVector populationWithFitness
+  (member0, fitness0) <- chooseVectorUniform populationWithFitness
+  (member1, fitness1) <- chooseVectorUniform populationWithFitness
   pure $ if fitness0 > fitness1 then member0 else member1
 
-chooseVector :: MonadRandom m => Vector a -> m a
-chooseVector items = Vector.unsafeIndex items <$> getRandomR (0, Vector.length items - 1)
+chooseVectorUniform :: MonadRandom m => Vector a -> m a
+chooseVectorUniform items = Vector.unsafeIndex items <$> getRandomR (0, Vector.length items - 1)
+
+chooseThreshold :: Double -> Vector (a, Double) -> a
+chooseThreshold threshold populationWithFitness =
+  case Vector.length populationWithFitness of
+    0 -> error "cannot choose from an empty vector"
+    1 -> fst $ Vector.unsafeIndex populationWithFitness 0
+    _ ->
+      let (a, weight) = Vector.unsafeIndex populationWithFitness 0
+       in if weight >= threshold
+            then a
+            else chooseThreshold (threshold - weight) (Vector.drop 1 populationWithFitness)
