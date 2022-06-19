6d66a753a8bcf433c25c08700b24644acd9caa44
jali-clarke <jinnah.ali-clarke@outlook.com>
2022-06-19 14:38:33 +0000

    add tournamentSelect strategy

---
+++ src/Genetic/Strategies.hs
@@ -0,0 +1,19 @@
+module Genetic.Strategies
+  ( tournamentSelect
+  )
+where
+
+import Control.Monad.Random.Class (MonadRandom (getRandomR))
+import Data.Vector (Vector)
+import qualified Data.Vector as Vector
+
+tournamentSelect :: MonadRandom m => Vector (a, Double) -> m a
+tournamentSelect populationWithFitness = do
+  (member0, fitness0) <- chooseVector populationWithFitness
+  (member1, fitness1) <- chooseVector populationWithFitness
+  pure $ if fitness0 > fitness1 then member0 else member1
+
+chooseVector :: MonadRandom m => Vector a -> m a
+chooseVector items = do
+  let numItems = Vector.length items
+  Vector.unsafeIndex items <$> getRandomR (0, numItems - 1)
